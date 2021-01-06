78208d71a3c4addf536898ce5d53ab86a5c42bbc
sylinx_zj <sylinx_zj@163.com>
2021-01-06 10:11:12 +0900

    1

--- src/main/java/com/cao/config/SecurityConfig.java
+++ src/main/java/com/cao/config/SecurityConfig.java
@@ -6,56 +6,32 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-/**
- * @Author: huahua
- * @Date: 2020-11-25 16:44
- */
-
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    /**
-     * 授权的
-     * @param http
-     * @throws Exception
-     */
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        //首页所有人可以访问，功能页有相应权限才能访问
-        //链式编程
-        //请求授权的规则
+
         http.authorizeRequests()
                 .antMatchers("/").permitAll()
-                .antMatchers("/level1/**").hasRole("vip1")
-                .antMatchers("/level2/**").hasRole("vip2")
-                .antMatchers("/level3/**").hasRole("vip3");
+                .antMatchers("/user/**").hasRole("user")
+                .antMatchers("/admin/**").hasRole("admin")
+                .antMatchers("/guest/**").hasRole("guest");
 
-        //没有权限，默认到登录页面
         http.formLogin().loginPage("/toLogin").usernameParameter("user").passwordParameter("pwd").loginProcessingUrl("/login");
-
-        //防止网站攻击
-        http.csrf().disable();//登出可能存在失败的原因
-        //注销功能
+        http.csrf().disable();
         http.logout().logoutSuccessUrl("/");
-        //开启记住我功能
         http.rememberMe().rememberMeParameter("remember");
 
     }
 
-    /**
-     * 认证的
-     * @param auth
-     * @throws Exception
-     */
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication().passwordEncoder(new BCryptPasswordEncoder())
-                .withUser("cjh").password(new BCryptPasswordEncoder().encode("123123")).roles("vip2","vip3")
+                .withUser("testuser").password(new BCryptPasswordEncoder().encode("123123")).roles("user")
                 .and()
-                .withUser("root").password(new BCryptPasswordEncoder().encode("123123")).roles("vip1","vip2","vip3")
+                .withUser("root").password(new BCryptPasswordEncoder().encode("123123")).roles("admin")
                 .and()
-                .withUser("guest").password(new BCryptPasswordEncoder().encode("123123")).roles("vip1");
-
-        //There is no PasswordEncoder mapped for the id "null"
-
+                .withUser("guest").password(new BCryptPasswordEncoder().encode("123123")).roles("guest");
     }
 }
--- src/main/java/com/cao/controller/RouterController.java
+++ src/main/java/com/cao/controller/RouterController.java
@@ -4,10 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-/**
- * @Author: huahua
- * @Date: 2020-11-25 16:26
- */
 @Controller
 public class RouterController {
 
@@ -21,19 +17,19 @@ public class RouterController {
         return "views/login";
     }
 
-    @RequestMapping("/level1/{id}")
-    public String level1(@PathVariable("id") int id) {
-        return "views/level1/" + id;
+    @RequestMapping("/user/{id}")
+    public String user(@PathVariable("id") int id) {
+        return "views/user/" + id;
     }
 
-    @RequestMapping("/level2/{id}")
-    public String level2(@PathVariable("id") int id) {
-        return "views/level2/" + id;
+    @RequestMapping("/admin/{id}")
+    public String admin(@PathVariable("id") int id) {
+        return "views/admin/" + id;
     }
 
-    @RequestMapping("/level3/{id}")
-    public String level3(@PathVariable("id") int id) {
-        return "views/level3/" + id;
+    @RequestMapping("/guest/{id}")
+    public String guest(@PathVariable("id") int id) {
+        return "views/guest/" + id;
     }
 
 }
